6b9dc047473a08412b71656de42147fd10084b06
Socks Candy <socksthefennec@gmail.com>
2023-12-15 17:48:22 +1000

    Disable autologin

--- systems/x86_64-linux/purgatory/user.nix
+++ systems/x86_64-linux/purgatory/user.nix
@@ -5,10 +5,10 @@
 }: let
   inherit (config) sockscfg;
 in {
-  services.xserver.displayManager.autoLogin = lib.mkIf config.services.xserver.enable {
-    enable = true;
-    user = sockscfg.user.name;
-  };
+  # services.xserver.displayManager.autoLogin = lib.mkIf config.services.xserver.enable {
+  #   enable = true;
+  #   user = sockscfg.user.name;
+  # };
   users.users.${sockscfg.user.name} = {
     initialHashedPassword = "$y$j9T$O663AsUwicdKYIQzKpqoV.$0VhavBNOGds5YLA.fBmASVR.PyWiE76Wgeh1QNG1LE4";
     extraGroups = ["networkmanager"];
